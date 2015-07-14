9a3b7d51b09fb155ace177bac36902ca3892a216
Martin Meredith <martin@sourceguru.net>
2015-07-14 23:43:15 +0100

    Basic Rainbowification

---
+++ rainbowify.js
@@ -0,0 +1,21 @@
+$('h1,h2,h3,h4').find('*:not(:has(*))').each(function (el) {
+        console.log(this);
+        if ($(this).text().trim() !== "") {
+            $(this).rainbow({
+                colors: [
+                    '#FF0000',
+                    '#f26522',
+                    '#fff200',
+                    '#00a651',
+                    '#28abe2',
+                    '#2e3192',
+                    '#6868ff'
+                ],
+                animate: true,
+                animateInterval: 100,
+                pad: false,
+                pauseLength: 100
+            })
+        }
+    }
+);
